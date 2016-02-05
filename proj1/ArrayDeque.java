4838ab92cba105856868dd91cd7c5a2bd3b9ca20
rohsuresh <rohansuresh@berkeley.edu>
2016-02-04 21:49:16 -0800

    Hope this works

---
+++ proj1/ArrayDeque.java
@@ -0,0 +1,208 @@
+public class ArrayDeque<Item> {
+
+    private Item[] items;
+    private int size;
+    private int nextFirst;
+    private int nextLast;
+    private int capacity;
+    private static int rFactor = 2;
+
+    public ArrayDeque() {
+        capacity = items.length;
+        items = (Item[]) new Object[8];
+        size = 0;
+        nextFirst= 0;
+        nextLast = 1;
+    }
+
+    public void resize(int newCapacity) {
+        Item[] newCopy = (Item[]) new Object[newCapacity];
+        Item[] temp = (Item[]) new Object[size];
+        int position = 0;
+        while (position < size){
+            temp[position] = (items[(nextFirst + 1 + position) % items.length]);
+            position++;
+        }
+        
+        System.arraycopy(temp, 0, newCopy, 0, size);
+        items = newCopy;
+        nextFirst = items.length - 1;
+        nextLast = size;
+
+    }
+
+    public boolean downsize() {
+        if ((size / items.length) < 0.25) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+
+    public void addFirst(Item x) {
+        // adds an item to the front
+        if (size == items.length) {
+            resize(size * rFactor);
+        }
+
+        items[nextFirst] = x;
+        if (nextFirst == 0) {
+            nextFirst = nextFirst - 1 + capacity; // loop around
+        }
+        else {
+            nextFirst--;
+        }
+    	size++;
+    }
+
+    public void addLast(Item x) {
+    	// adds an item to the back
+        if (size == items.length) {
+            resize(size * rFactor);
+        }
+        
+        items[nextLast] = x;
+        if (nextLast == capacity - 1) {
+            nextLast = 0;
+        }
+        else {
+            nextLast++;
+        }
+        size++;
+    }
+
+    public boolean isEmpty() {
+    	// returns true if deque is empty
+        if (size == 0) {
+            return true;
+        }
+        return false;
+    }
+
+    public int size() {
+    	// returns num items in deque
+        return size;
+    }
+
+    public void printDeque() {
+    	// prints items from first to last separated by spaces
+        if (isEmpty()) {
+            System.out.println(); // empty, nothing in deque
+        }
+        else {
+            int position = 0;
+            while (position < size) {
+                System.out.print(items[(nextFirst + 1 + position) % items.length] + " ");
+                position++; 
+            }
+        }   
+  }
+
+
+    public Item removeFirst() {
+    	// removes and returns item at front
+        int removeFirstTrack;
+        Item itemToRemove;
+        if (nextFirst == capacity - 1) {
+            removeFirstTrack = 0;
+        }
+        else {
+            removeFirstTrack = nextFirst + 1;
+        }
+        
+        if (removeFirstTrack == 0) {
+            itemToRemove = items[removeFirstTrack];
+            items[removeFirstTrack] = null;
+            nextFirst = 0; // very front is empty
+            size--;
+            if (downsize()) {
+                resize(size / rFactor);
+            }
+        }
+        else {
+            itemToRemove = items[removeFirstTrack];
+            items[removeFirstTrack] = null;
+            nextFirst++;
+            size--;
+            if (downsize()) {
+                resize(size / rFactor);
+            }
+
+        }
+        return itemToRemove;
+    }
+
+    public Item removeLast() {
+    	// removes and returns item at back
+        int removeLastTrack;
+        Item itemToRemove;
+        if (nextLast == 0) {
+            removeLastTrack = capacity - 1;
+        }
+        else {
+            removeLastTrack = nextLast - 1;
+        }
+       
+        if (removeLastTrack == capacity - 1) {
+            itemToRemove = items[removeLastTrack];
+            items[removeLastTrack] = null;
+            nextLast = capacity - 1;
+            size--;
+            if (downsize()) {
+                resize(size / rFactor);
+            }
+        }
+        else {
+            itemToRemove = items[removeLastTrack];
+            items[removeLastTrack] = null;
+            nextLast--;
+            size--;
+            if (downsize()) {
+                resize(size / rFactor);
+            }
+        }
+        return itemToRemove;
+    }
+
+    public Item get(int index){
+        if (index < size){
+            return items[(nextFirst + 1 + index) % items.length];
+        }
+        else {
+            return null;
+        }
+  }
+}
+
+
+
+    /*private static void main (String[] args) {
+        ArrayDeque testArray = new ArrayDeque();
+        testArray.addFirst(0);
+        testArray.addFirst(7);
+        testArray.addLast(1);
+        testArray.printDeque(); // 7 0 1
+        System.out.println();
+        System.out.println(testArray.get(2)); // 1
+        System.out.println(testArray.removeFirst()); // 7
+        System.out.println(testArray.removeLast()); // 1 
+        testArray.printDeque(); // 0
+        System.out.println();
+        System.out.println(testArray.size()); // 1
+        System.out.println(testArray.isEmpty()); // false
+        testArray.addFirst("second");
+        testArray.addFirst("first");
+        testArray.addLast(1);
+        testArray.addLast(2);
+        testArray.addLast(3);
+        testArray.printDeque(); // first second 0 1 2 3
+        System.out.println();
+        System.out.println(testArray.size());
+        testArray.addLast(4);
+        testArray.addLast(5);
+        testArray.addFirst(6);
+        testArray.printDeque();*/
+    
+
---
+++ proj1/LinkedListDeque.java
@@ -0,0 +1,161 @@
+public class LinkedListDeque<Item> {
+	
+	public class Node {
+		public Node prev;
+        public Item item;
+		public Node next;
+
+		public Node (Node p, Item x, Node n) {
+			// parameters: Node, previous Node, next Node
+			prev = p;
+            item = x;
+			next = n;
+		}
+	}
+
+	private Node sentinel;
+	private int size;
+	
+	public LinkedListDeque() {
+		// empty linked list deque
+		size = 0;
+		sentinel = new Node(sentinel, null, sentinel);
+        sentinel.prev = sentinel;
+        sentinel.next = sentinel;
+		// in an empty list previous and next are sentinel itself
+	}
+
+    public void addFirst(Item x) {
+    	// adds an Node to the front
+    	// no recursion or iteration
+    	Node oldFirstNode = sentinel.next;
+    	Node newFirstNode = new Node(sentinel, x, oldFirstNode);
+    	oldFirstNode.prev = newFirstNode;
+        sentinel.next = newFirstNode;
+    	size += 1;
+    }
+
+    public void addLast(Item x) {
+    	// adds an Node to the back
+    	// no recursion or iteration
+    	Node oldLastNode = sentinel.prev;
+    	Node newLastNode = new Node(oldLastNode, x, sentinel);
+        oldLastNode.next = newLastNode;
+        sentinel.prev = newLastNode;
+        size += 1;    	
+    }
+
+    public boolean isEmpty() {
+    	// returns true if deque is empty
+        if (sentinel.next == sentinel) {
+            // only sentinel is present and in circular strcuture
+            size = 0;
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    public int size() {
+    	// returns num Nodes in deque
+    	// constant time
+        return size;
+    }
+
+    public void printDeque() {
+    	// prints items from first to last separated by spaces
+        int counter = 0;
+        Node nodeToPrint = sentinel.next;
+        while (counter < size) {
+            System.out.print(nodeToPrint.item + " ");
+            nodeToPrint = nodeToPrint.next;
+            counter += 1;
+        }
+    }
+
+    public Item removeFirst() {
+    	// removes and returns Node at front
+    	// no recursion or iteration
+        
+        Node nodeToBeRemoved = sentinel.next;
+        sentinel.next = nodeToBeRemoved.next;
+        nodeToBeRemoved.next.prev = sentinel;
+        // now no references pointing to the Node
+        
+        size = size - 1;
+        return nodeToBeRemoved.item;
+    }
+
+    public Item removeLast() {
+    	// removes and returns Node at back
+    	// no recursion or iteration
+        
+        Node nodeToBeRemoved = sentinel.prev;
+        sentinel.prev = nodeToBeRemoved.prev;
+        nodeToBeRemoved.prev.next = sentinel;
+        // now no reference pointing to the Node
+        
+        size = size - 1;
+        return nodeToBeRemoved.item;
+    }
+
+    public Item get(int index) {
+    	// gets Node at given index, doesn't change the linked list
+    	// use iteration
+        if (index >= size) {
+            return null;
+        }
+        
+        int counter = 0;
+        Node nodeToGet = sentinel.next;
+        while (counter < index) {
+            nodeToGet = nodeToGet.next;
+            counter += 1;
+        }
+        return nodeToGet.item;
+    }
+
+    public Item getRecursive(int index) {
+		// recursive get method
+        if (index >= size) {
+            return null;
+        }
+        
+        return getRecursiveHelper(index, sentinel.next);
+	}
+
+    private Item getRecursiveHelper(int index, Node position) {
+        if (index == 0) {
+            return position.item;
+        }
+        else {
+            return getRecursiveHelper(index - 1, position.next);
+        }
+    }
+}
+
+
+    /*public static void main (String[] args) {
+        LinkedListDeque testLink = new LinkedListDeque();
+        // series of methods run for testing
+        testLink.addFirst(2);
+        testLink.addFirst(1);
+        testLink.addLast(3);
+        testLink.addLast(4);
+        testLink.printDeque();
+        System.out.println();
+        System.out.println(testLink.get(2)); // should retrieve the integer 3
+        System.out.println(testLink.getRecursive(3)); // should retrieve the integer 4
+        System.out.println(testLink.size()); // should be 4
+        System.out.println(testLink.removeLast()); // should be 4
+        System.out.println(testLink.removeFirst()); // should be 1
+        System.out.println(testLink.size()); // should be 2
+        testLink.printDeque();
+        System.out.println();
+        System.out.println(testLink.get(4)); // should be null
+        System.out.println(testLink.getRecursive(5)); // should be null
+        System.out.println(testLink.isEmpty()); // should be false
+
+        
+    }*/
